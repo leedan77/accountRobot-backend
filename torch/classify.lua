58c99475c2e28684fefa61c305c8137a277c2a05
tangsttw <auop.321@gmail.com>
2016-10-16 14:29:09 +0800

    fix classify.lua problem without open image file

--- torch/classify.lua
+++ torch/classify.lua
@@ -21,9 +21,9 @@ io.output(file)
 --print '==> Downloading image and network'
 local image_url = arg[1]--'http://upload.wikimedia.org/wikipedia/commons/e/e9/Goldfish3.jpg'
 local network_url = 'https://www.dropbox.com/s/npmr5egvjbg7ovb/nin_nobn_final.t7'
-local image_name = paths.basename(image_url)
+local image_name = string.sub (paths.basename(image_url), 1, 4)
 local network_name = paths.basename(network_url)
-if not paths.filep(image_name) then os.execute('wget '..image_url)   end
+if not paths.filep(image_name) then os.execute('wget ' .. image_url .. ' -O new' .. image_name .. '.jpg')   end
 if not paths.filep(network_name) then os.execute('wget '..network_url)   end
 
 --print '==> Loading network'
@@ -37,7 +37,7 @@ local synset_words = {}
 for line in io.lines'synset_words.txt' do table.insert(synset_words, line:sub(11)) end
 
 --print '==> Loading image and imagenet mean'
-local im = image.load(image_name)
+local im = image.load('new'.. image_name .. '.jpg')
 
 --print '==> Preprocessing'
 -- Our network has mean and std saved in net.transform
